6a96a0bfe712fecbb396b137706160e8c6720fc5
Patricio Albornoz <83987715+pato-1441@users.noreply.github.com>
2023-06-08 19:54:53 -0300

    Actividad Promedio #1 Finalizada

---
+++ ActividadPromedio/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = -1;
+            int valor;
+
+            Promedio promedio = new Promedio();
+
+            while (opcion != 3)
+            {
+                Console.WriteLine("1 - Agregar valor. \n2 - Calcular promedio.\n3 - Salir.");
+                opcion = Convert.ToInt32(Console.ReadLine());
+                switch (opcion)
+                {
+                    case 1: Console.WriteLine("Ingrese un valor: ");
+                            valor = Convert.ToInt32(Console.ReadLine());
+                            promedio.AgregarValor(valor);
+                            break;
+                    case 2: Console.WriteLine("Promedio: {0}", promedio.CalcularPromedio());
+                            break;
+                    case 3: Console.WriteLine("Presione una tecla para salir.");
+                            Console.ReadKey();
+                            break;
+                    default:
+                            break;
+                }
+            }
+        }
+    }
+}
---
+++ ActividadPromedio/Promedio.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Promedio
+    {
+        public Promedio(){}
+        int contador = 0;
+        int acumulador = 0;
+
+
+        public void AgregarValor(int valor)
+        {
+            contador++;
+            acumulador += valor;
+        }
+
+        public double CalcularPromedio()
+        {
+            if (contador > 0)
+            {
+                return (double)acumulador / contador;
+            } else
+            {
+                return 0;
+            }
+        }
+    }
+}
